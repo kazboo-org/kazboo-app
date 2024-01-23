80dc168a27ee7b49c24456ded51ad92ce2c57527
BoroDinS <sergey.borodin.lg@gmail.com>
2024-01-23 18:00:39 +0100

    add post options button, to interact with a post

--- components/common/Post/Post.style.js
+++ components/common/Post/Post.style.js
@@ -10,11 +10,47 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowOffset: 0.3,
   },
-  headerContainer: { flexDirection: "row" },
+  headerContainer: { flexDirection: "row", marginBottom: 10 },
+  headerInfoContainer: { justifyContent: "center", marginLeft: 10 },
   title: {
-    textTransform: "uppercase",
     fontWeight: "bold",
-    marginBottom: 5,
+  },
+  postDate: { fontWeight: "300" },
+  postOptionsButton: { marginLeft: "auto" },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    // color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
 
--- components/common/Post/Post.tsx
+++ components/common/Post/Post.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  Pressable,
+  Button,
+  Modal,
+  Alert,
+} from "react-native";
 import TagList from "./TagsList";
 import styles from "./Post.style";
 import HeartBadge from "../UI/HeartBadge/HeartBadge";
+import { Link } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Post({ post }: any) {
+  const [showPostOptions, setShowPostOptions] = useState(false);
+
   // This function sets a heart badge randomly on a user profile photo.
   // Later, the heart badge will be set according to the user data fetched from the database and set in the global state.
   function getRandomBooleanForSetHeartBadge() {
@@ -12,20 +24,59 @@ export default function Post({ post }: any) {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <View>
-          <Image
-            source={require("../../../assets/images/userPhoto.png")}
-            style={{ height: 47, width: 47 }}
-          />
-          {getRandomBooleanForSetHeartBadge() && <HeartBadge />}
+    <>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View>
+            <Image
+              source={require("../../../assets/images/userPhoto.png")}
+              style={{ height: 47, width: 47 }}
+            />
+            {getRandomBooleanForSetHeartBadge() && <HeartBadge />}
+          </View>
+          <View style={styles.headerInfoContainer}>
+            <Text style={styles.title}>JohnDoe</Text>
+            <Text style={styles.postDate}>22. januar 2024</Text>
+          </View>
+          <Pressable
+            onPress={() => setShowPostOptions(true)}
+            style={[styles.button, { marginLeft: "auto" }]}
+          >
+            {({ pressed }) => (
+              <FontAwesome
+                name="ellipsis-v"
+                size={25}
+                color={"black"}
+                style={{ opacity: pressed ? 0.5 : 1 }}
+              />
+            )}
+          </Pressable>
         </View>
-        <Text style={styles.title}>JohnDoe</Text>
+        <Text>{post.body}</Text>
+        <TagList tags={post.tags} />
+        <Text>Reactions: {post.reactions}</Text>
       </View>
-      <Text>{post.body}</Text>
-      <TagList tags={post.tags} />
-      <Text>Reactions: {post.reactions}</Text>
-    </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showPostOptions}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setShowPostOptions(!showPostOptions);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setShowPostOptions(!showPostOptions)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
