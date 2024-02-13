0e7a30fa36fd241f92a7bbd12d8cadf06e7849f3
BoroDinS <sergey.borodin.lg@gmail.com>
2024-02-13 15:24:51 +0100

    download pictures, create a simple version of a card for user profile dashboard

--- app/userProfileDashboard.style.js
+++ app/userProfileDashboard.style.js
@@ -5,6 +5,12 @@ const styles = StyleSheet.create({
   screenDescriptionContainer: { marginHorizontal: 15, gap: 10 },
   screenTitle: { fontSize: 32, fontWeight: "900" },
   screenDescription: { fontSize: 22, fontWeight: "200" },
+  cardsContainer: { gridRowStart: "", gap: 15 },
+  card: {
+    padding: 7,
+    width: "47%",
+    backgroundColor: "tomato",
+  },
 });
 
 export default styles;
--- app/userProfileDashboard.tsx
+++ app/userProfileDashboard.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import Button from "../components/common/Button/Button";
 import styles from "./userProfileDashboard.style";
 import SpeechButton from "../components/common/SpeechButton/SpeechButton";
+import { Link } from "expo-router";
 
 const screenDescription =
   "Her kan du ændre på hvordan din profil ser ud, hvordan du vil have notifikationer og få hjælp.";
@@ -15,6 +16,40 @@ export default function UserProfileDashboard() {
         <Text style={styles.screenTitle}>Dine indstillinger</Text>
         <Text style={styles.screenDescription}> {screenDescription}</Text>
         <SpeechButton speechText={screenDescription} />
+      </View>
+      <View style={styles.cardsContainer}>
+        <View style={styles.card}>
+          <Link href="/notifications" asChild>
+            <Image
+              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+              style={{ width: 157, height: 157 }}
+            />
+          </Link>
+        </View>
+        <View style={styles.card}>
+          <Link href="/notifications" asChild>
+            <Image
+              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+              style={{ width: 157, height: 157 }}
+            />
+          </Link>
+        </View>
+        <View style={styles.card}>
+          <Link href="/notifications" asChild>
+            <Image
+              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+              style={{ width: 157, height: 157 }}
+            />
+          </Link>
+        </View>
+        <View style={styles.card}>
+          <Link href="/notifications" asChild>
+            <Image
+              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+              style={{ width: 157, height: 157 }}
+            />
+          </Link>
+        </View>
       </View>
     </View>
   );
