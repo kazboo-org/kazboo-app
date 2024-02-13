9d5695b194f385288aa340a7d74b1d4349e35cfc
BoroDinS <sergey.borodin.lg@gmail.com>
2024-02-13 20:12:17 +0100

    create metrics helper file for improving responsiveness, make a simple grid of cards for the user profile dashboard

--- app/userProfileDashboard.style.js
+++ app/userProfileDashboard.style.js
@@ -1,16 +1,39 @@
 import { StyleSheet } from "react-native";
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from "../constants/Metrics";
 
 const styles = StyleSheet.create({
-  screen: { paddingVertical: 30, paddingHorizontal: 15, gap: 30 },
-  screenDescriptionContainer: { marginHorizontal: 15, gap: 10 },
-  screenTitle: { fontSize: 32, fontWeight: "900" },
-  screenDescription: { fontSize: 22, fontWeight: "200" },
-  cardsContainer: { gridRowStart: "", gap: 15 },
-  card: {
-    padding: 7,
-    width: "47%",
-    backgroundColor: "tomato",
+  screen: {
+    paddingVertical: verticalScale(30),
+    paddingHorizontal: "2%",
+    backgroundColor: "rgba(0,0,0,0.05)",
   },
+  screenDescriptionContainer: {
+    marginHorizontal: horizontalScale(20),
+    marginVertical: verticalScale(15),
+    gap: 10,
+  },
+  screenTitle: { fontSize: moderateScale(32), fontWeight: "900" },
+  screenDescription: { fontSize: 22, fontWeight: "200" },
+  cardsContainer: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+  },
+  cardContainer: {
+    borderRadius: moderateScale(10),
+    paddingTop: verticalScale(13),
+    paddingHorizontal: horizontalScale(8),
+    margin: "2%",
+    paddingBottom: horizontalScale(10),
+    width: "46%",
+    backgroundColor: "white",
+  },
+  cardContent: { alignItems: "center" },
+
+  cardTitle: { fontSize: moderateScale(24), marginVertical: verticalScale(10) },
 });
 
 export default styles;
--- app/userProfileDashboard.tsx
+++ app/userProfileDashboard.tsx
@@ -1,56 +1,99 @@
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  useColorScheme,
+  Pressable,
+} from "react-native";
 import React from "react";
 import Button from "../components/common/Button/Button";
 import styles from "./userProfileDashboard.style";
 import SpeechButton from "../components/common/SpeechButton/SpeechButton";
 import { Link } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Colors from "../constants/Colors";
 
 const screenDescription =
   "Her kan du ændre på hvordan din profil ser ud, hvordan du vil have notifikationer og få hjælp.";
 
 export default function UserProfileDashboard() {
+  const colorScheme = useColorScheme();
+
   return (
-    <View style={styles.screen}>
+    <ScrollView style={styles.screen}>
       <Button buttonText="Se min profil" path="/userProfile" />
       <View style={styles.screenDescriptionContainer}>
         <Text style={styles.screenTitle}>Dine indstillinger</Text>
-        <Text style={styles.screenDescription}> {screenDescription}</Text>
+        <Text style={styles.screenDescription}>{screenDescription}</Text>
         <SpeechButton speechText={screenDescription} />
       </View>
       <View style={styles.cardsContainer}>
-        <View style={styles.card}>
-          <Link href="/notifications" asChild>
-            <Image
-              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
-              style={{ width: 157, height: 157 }}
-            />
+        <View style={styles.cardContainer}>
+          <Link href="/home">
+            <View style={styles.cardContent}>
+              <Image
+                source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+                style={{ width: 157, height: 157 }}
+              />
+              <FontAwesome
+                name="heart"
+                size={40}
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <Text style={styles.cardTitle}>Kærester</Text>
+            </View>
           </Link>
         </View>
-        <View style={styles.card}>
-          <Link href="/notifications" asChild>
-            <Image
-              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
-              style={{ width: 157, height: 157 }}
-            />
+        <View style={styles.cardContainer}>
+          <Link href="/notifications">
+            <View style={styles.cardContent}>
+              <Image
+                source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+                style={{ width: 157, height: 157 }}
+              />
+              <FontAwesome
+                name="heart"
+                size={40}
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <Text style={styles.cardTitle}>Kærester</Text>
+            </View>
           </Link>
         </View>
-        <View style={styles.card}>
-          <Link href="/notifications" asChild>
-            <Image
-              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
-              style={{ width: 157, height: 157 }}
-            />
+        <View style={styles.cardContainer}>
+          <Link href="/notifications">
+            <View style={styles.cardContent}>
+              <Image
+                source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+                style={{ width: 157, height: 157 }}
+              />
+              <FontAwesome
+                name="heart"
+                size={40}
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <Text style={styles.cardTitle}>Kærester</Text>
+            </View>
           </Link>
         </View>
-        <View style={styles.card}>
-          <Link href="/notifications" asChild>
-            <Image
-              source={require("../assets/images/user-profile/havebilledde-2.jpg")}
-              style={{ width: 157, height: 157 }}
-            />
+        <View style={styles.cardContainer}>
+          <Link href="/notifications">
+            <View style={styles.cardContent}>
+              <Image
+                source={require("../assets/images/user-profile/havebilledde-2.jpg")}
+                style={{ width: 157, height: 157 }}
+              />
+              <FontAwesome
+                name="heart"
+                size={40}
+                color={Colors[colorScheme ?? "light"].text}
+              />
+              <Text style={styles.cardTitle}>Kærester</Text>
+            </View>
           </Link>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
